9879000e49f5bac84b281d3a2d84a0ee1a8ed6d3
HarrelsonJ <34192484+HarrelsonJ@users.noreply.github.com>
2017-12-16 09:01:30 -0800

    Setting up as a class

--- Testing.py
+++ Testing.py
@@ -1,14 +1,21 @@
-file = open("TestFile.py","w")
-
-file.write("Hello, World!\n")
-file.write("This, is, our, new, text, file., \n")
-file.write("and, this, is, another, line., \n")
-file.write("Why?, Because, we, can., \n")
-file.close()
-
-with open("TestFile.py", "r") as f:
-    data = f.readlines()
-    for line in data:
-        words = line.split()
-        print(words)
-file.close()
+class Interpreter:
+    def Interprate(FileName):
+        str(FileName)
+        file = open(FileName,"r")
+        masterList = []
+        with open(FileName) as f:
+            data = f.readlines()
+            for line in data:
+                temp = line.split(", ")
+                temp.remove("\n")
+                masterList.append(temp)
+        file.close()
+        for i in masterList:
+            if i[0] == "1":
+                i[0] = "driveCommand"
+            elif i[0] == "2":
+                i[0] = "turnCommand"
+            elif i[0] == "3":
+                i[0] = "genericCommand"
+        return masterList
+    print(Interpreter.Interprate("TestFile.py"))
